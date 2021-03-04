0e3deacd43279702c07bad267277c42cfc058769
rfernandez <rfernandez@tuenti.com>
2021-03-04 01:25:27 +0100

    Keep everything inside backup script, remove > from crontab

--- backup.sh
+++ backup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 source $HOME/.bash_profile
-date=$(date '+%Y-%m-%d-%H%M%S')
-docker exec mongo sh -c "mongodump --authenticationDatabase admin --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --db $MONGO_INITDB_DATABASE --gzip --archive"
-mv backup.gz $date-backup.gz
+date=$(date '+%Y-%m-%d')
+docker exec mongo sh -c 'exec mongodump --authenticationDatabase admin --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --db $MONGO_INITDB_DATABASE --gzip --archive' > $date.gz
 git add .
 git commit -am "$date Backup"
 git push
